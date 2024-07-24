0b7ce35b995a2d1219d4e939a5a78b04a1f853b3
yvesd <yves.daetwyler@gmail.com>
2024-07-24 20:00:27 +0200

    improve local img src handling (use database pathes in component)

--- src/resources/js/components/Card.jsx
+++ src/resources/js/components/Card.jsx
@@ -1,48 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 
-const Card = ({ card, source, scrollPosition }) => {
-    const [imgUrl, setImgUrl] = useState({})
-    const [fallback, setFallback] = useState('')
-
-    useEffect(() => {
-        const cardObjImageUris = JSON.parse(card['image_uris']);
-        setFallback(cardObjImageUris.large)
-        if (source === 'api') {
-            const imgUrlObj = {
-                small: cardObjImageUris ? cardObjImageUris.small : '',
-                normal:  cardObjImageUris ? cardObjImageUris.normal : '',
-                large:  cardObjImageUris ? cardObjImageUris.large : '',
+// 'image_uris' is external (when fetched from API and not yet saved locally)
+// 'imgs_local' is the local images
+const Card = ({ card, source, scrollPosition }) => (
+    <picture className="m-8 max-w-sm shadow-xl shadow-indigo-200 leading-none">
+        <LazyLoadImage
+            alt={card.name}
+            effect="blur"
+            scrollPosition={scrollPosition}
+            src={source === 'api' ? JSON.parse(card['image_uris']).large : 'storage/' + card['imgs_local'].large}
+            srcSet={`
+                ${source === 'api' ? JSON.parse(card['image_uris']).small : 'storage/' + card['imgs_local'].small} 300w,
+                ${source === 'api' ? JSON.parse(card['image_uris']).normal : 'storage/' + card['imgs_local'].normal} 768w,
+                ${source === 'api' ? JSON.parse(card['image_uris']).large : 'storage/' + card['imgs_local'].large} 1280w`
             }
-            setImgUrl(imgUrlObj)
-        } else {
-            const imgUrlObj = {
-                small: `/storage/images/${card.scryfall_id}/small.jpg`,
-                normal: `/storage/images/${card.scryfall_id}/normal.jpg`,
-                large: `/storage/images/${card.scryfall_id}/large.jpg`,
-            }
-            setImgUrl(imgUrlObj)
-        }
-    }, []);
-
-    return (
-        <picture className="m-8 max-w-sm shadow-xl shadow-indigo-200 leading-none">
-            <LazyLoadImage
-                alt={card.name}
-                effect="blur"
-                scrollPosition={scrollPosition}
-                src={imgUrl.large}
-                srcSet={`
-                ${imgUrl.small} 300w,
-                ${imgUrl.normal} 768w,
-                ${imgUrl.large} 1280w`
-                }
-                sizes="(max-width: 300px) 300px, (max-width: 768px) 768px, 1280px"
-                onError={e => {e.target.onError = null; e.target.src= fallback}}
-            />
-        </picture>
-    )
-}
+            sizes="(max-width: 300px) 300px, (max-width: 768px) 768px, 1280px"
+            onError={e => {e.target.onError = null; JSON.parse(card['image_uris']).large}}
+        />
+    </picture>
+)
 
 export default Card
