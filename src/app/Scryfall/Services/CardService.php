af7859ad472e68e260383be552db8a31847c88b7
yvesd <yves.daetwyler@gmail.com>
2024-07-24 23:41:55 +0200

    fetch all cards (response is paginated and includes max 175 cards)

--- src/app/Scryfall/Services/CardService.php
+++ src/app/Scryfall/Services/CardService.php
@@ -18,86 +18,8 @@ class CardService implements CardServiceInterface
     {
         if (!$this->cardsExistInDb($setCode)) {
             try {
-                $response = Http::get("https://api.scryfall.com/cards/search?q=set:{$setCode}");
-
-                if ($response->successful()) {
-                    $cards = $response->json()['data'];
-
-                    foreach ($cards as $cardData) {
-                        $data = [
-                            'scryfall_id' => $cardData['id'] ?? null,
-                            'oracle_id' => $cardData['oracle_id'] ?? null,
-                            'multiverse_ids' => isset($cardData['multiverse_ids']) ? json_encode($cardData['multiverse_ids']) : null,
-                            'name' => $cardData['name'] ?? null,
-                            'lang' => $cardData['lang'] ?? null,
-                            'released_at' => $cardData['released_at'] ?? null,
-                            'uri' => $cardData['uri'] ?? null,
-                            'scryfall_uri' => $cardData['scryfall_uri'] ?? null,
-                            'layout' => $cardData['layout'] ?? null,
-                            'highres_image' => $cardData['highres_image'] ?? null,
-                            'image_status' => $cardData['image_status'] ?? null,
-                            // Some cards have image_uris inside card_faces
-                            'image_uris' => (isset($cardData['card_faces'][0]['image_uris']))
-                                ? json_encode($cardData['card_faces'][0]['image_uris'])
-                                : (isset($cardData['image_uris']) ? json_encode($cardData['image_uris']) : null),
-                            'mana_cost' => $cardData['mana_cost'] ?? null,
-                            'cmc' => $cardData['cmc'] ?? null,
-                            'type_line' => $cardData['type_line'] ?? null,
-                            'oracle_text' => $cardData['oracle_text'] ?? null,
-                            'colors' => isset($cardData['colors']) ? json_encode($cardData['colors']) : null,
-                            'color_identity' => isset($cardData['color_identity']) ? json_encode($cardData['color_identity']) : null,
-                            'keywords' => isset($cardData['keywords']) ? json_encode($cardData['keywords']) : null,
-                            'legalities' => isset($cardData['legalities']) ? json_encode($cardData['legalities']) : null,
-                            'games' => isset($cardData['games']) ? json_encode($cardData['games']) : null,
-                            'reserved' => $cardData['reserved'] ?? null,
-                            'foil' => $cardData['foil'] ?? null,
-                            'nonfoil' => $cardData['nonfoil'] ?? null,
-                            'finishes' => isset($cardData['finishes']) ? json_encode($cardData['finishes']) : null,
-                            'oversized' => $cardData['oversized'] ?? null,
-                            'promo' => $cardData['promo'] ?? null,
-                            'reprint' => $cardData['reprint'] ?? null,
-                            'variation' => $cardData['variation'] ?? null,
-                            'set_id' => $cardData['set_id'] ?? null,
-                            'set' => $cardData['set'] ?? null,
-                            'set_name' => $cardData['set_name'] ?? null,
-                            'set_type' => $cardData['set_type'] ?? null,
-                            'set_uri' => $cardData['set_uri'] ?? null,
-                            'set_search_uri' => $cardData['set_search_uri'] ?? null,
-                            'scryfall_set_uri' => $cardData['scryfall_set_uri'] ?? null,
-                            'rulings_uri' => $cardData['rulings_uri'] ?? null,
-                            'prints_search_uri' => $cardData['prints_search_uri'] ?? null,
-                            'collector_number' => $cardData['collector_number'] ?? null,
-                            'digital' => $cardData['digital'] ?? null,
-                            'rarity' => $cardData['rarity'] ?? null,
-                            'flavor_text' => $cardData['flavor_text'] ?? null,
-                            'card_back_id' => $cardData['card_back_id'] ?? null,
-                            'artist' => $cardData['artist'] ?? null,
-                            'artist_ids' => isset($cardData['artist_ids']) ? json_encode($cardData['artist_ids']) : null,
-                            'illustration_id' => $cardData['illustration_id'] ?? null,
-                            'border_color' => $cardData['border_color'] ?? null,
-                            'frame' => $cardData['frame'] ?? null,
-                            'security_stamp' => $cardData['security_stamp'] ?? null,
-                            'full_art' => $cardData['full_art'] ?? null,
-                            'textless' => $cardData['textless'] ?? null,
-                            'booster' => $cardData['booster'] ?? null,
-                            'story_spotlight' => $cardData['story_spotlight'] ?? null,
-                            'edhrec_rank' => $cardData['edhrec_rank'] ?? null,
-                            'preview' => isset($cardData['preview']) ? json_encode($cardData['preview']) : null,
-                            'prices' => isset($cardData['prices']) ? json_encode($cardData['prices']) : null,
-                            'related_uris' => isset($cardData['related_uris']) ? json_encode($cardData['related_uris']) : null,
-                            'purchase_uris' => isset($cardData['purchase_uris']) ? json_encode($cardData['purchase_uris']) : null,
-                            'imgs_local' => ['small' => '', 'normal' => '', 'large' => ''],
-                        ];
-
-                        Card::updateOrCreate(['oracle_id' => $cardData['oracle_id']], array_filter($data));
-
-                        $imageUris = $cardData['card_faces'][0]['image_uris'] ?? ($cardData['image_uris'] ?? []);
-
-                        $this->saveImagesAsync($imageUris, $cardData['oracle_id']);
-                    }
-                } else {
-                    Log::error('Failed to fetch cards from Scryfall', ['setCode' => $setCode, 'response' => $response->body()]);
-                }
+                $initialUrl = "https://api.scryfall.com/cards/search?q=set:{$setCode}";
+                $this->fetchAndSaveCardsFromUrl($initialUrl);
             } catch (\Exception $e) {
                 Log::error('Error fetching cards', ['setCode' => $setCode, 'message' => $e->getMessage()]);
             }
@@ -105,6 +27,116 @@ class CardService implements CardServiceInterface
             return Card::where('set', $setCode)->get();
         }
         return false;
+    }
+
+    /**
+     * Recursive function to fetch and save cards from a given URL.
+     *
+     * @param string $url
+     * @return void
+     */
+    private function fetchAndSaveCardsFromUrl(string $url): void
+    {
+        try {
+            $response = Http::get($url);
+
+            if ($response->successful()) {
+                $data = $response->json();
+                $cards = $data['data'];
+
+                foreach ($cards as $cardData) {
+                    $this->saveCardData($cardData);
+                }
+
+                if (isset($data['next_page'])) {
+                    $this->fetchAndSaveCardsFromUrl($data['next_page']);
+                }
+            } else {
+                Log::error('Failed to fetch cards from Scryfall', ['url' => $url, 'response' => $response->body()]);
+            }
+        } catch (\Exception $e) {
+            Log::error('Error fetching cards from URL', ['url' => $url, 'message' => $e->getMessage()]);
+        }
+    }
+
+    /**
+     * Save card data to the database and dispatch jobs to save images.
+     *
+     * @param array $cardData
+     * @return void
+     */
+    private function saveCardData(array $cardData): void
+    {
+        $data = [
+            'scryfall_id' => $cardData['id'] ?? null,
+            'oracle_id' => $cardData['oracle_id'] ?? null,
+            'multiverse_ids' => isset($cardData['multiverse_ids']) ? json_encode($cardData['multiverse_ids']) : null,
+            'name' => $cardData['name'] ?? null,
+            'lang' => $cardData['lang'] ?? null,
+            'released_at' => $cardData['released_at'] ?? null,
+            'uri' => $cardData['uri'] ?? null,
+            'scryfall_uri' => $cardData['scryfall_uri'] ?? null,
+            'layout' => $cardData['layout'] ?? null,
+            'highres_image' => $cardData['highres_image'] ?? null,
+            'image_status' => $cardData['image_status'] ?? null,
+            // Some cards have image_uris inside card_faces
+            'image_uris' => (isset($cardData['card_faces'][0]['image_uris']))
+                ? json_encode($cardData['card_faces'][0]['image_uris'])
+                : (isset($cardData['image_uris']) ? json_encode($cardData['image_uris']) : null),
+            'mana_cost' => $cardData['mana_cost'] ?? null,
+            'cmc' => $cardData['cmc'] ?? null,
+            'type_line' => $cardData['type_line'] ?? null,
+            'oracle_text' => $cardData['oracle_text'] ?? null,
+            'colors' => isset($cardData['colors']) ? json_encode($cardData['colors']) : null,
+            'color_identity' => isset($cardData['color_identity']) ? json_encode($cardData['color_identity']) : null,
+            'keywords' => isset($cardData['keywords']) ? json_encode($cardData['keywords']) : null,
+            'legalities' => isset($cardData['legalities']) ? json_encode($cardData['legalities']) : null,
+            'games' => isset($cardData['games']) ? json_encode($cardData['games']) : null,
+            'reserved' => $cardData['reserved'] ?? null,
+            'foil' => $cardData['foil'] ?? null,
+            'nonfoil' => $cardData['nonfoil'] ?? null,
+            'finishes' => isset($cardData['finishes']) ? json_encode($cardData['finishes']) : null,
+            'oversized' => $cardData['oversized'] ?? null,
+            'promo' => $cardData['promo'] ?? null,
+            'reprint' => $cardData['reprint'] ?? null,
+            'variation' => $cardData['variation'] ?? null,
+            'set_id' => $cardData['set_id'] ?? null,
+            'set' => $cardData['set'] ?? null,
+            'set_name' => $cardData['set_name'] ?? null,
+            'set_type' => $cardData['set_type'] ?? null,
+            'set_uri' => $cardData['set_uri'] ?? null,
+            'set_search_uri' => $cardData['set_search_uri'] ?? null,
+            'scryfall_set_uri' => $cardData['scryfall_set_uri'] ?? null,
+            'rulings_uri' => $cardData['rulings_uri'] ?? null,
+            'prints_search_uri' => $cardData['prints_search_uri'] ?? null,
+            'collector_number' => $cardData['collector_number'] ?? null,
+            'digital' => $cardData['digital'] ?? null,
+            'rarity' => $cardData['rarity'] ?? null,
+            'flavor_text' => $cardData['flavor_text'] ?? null,
+            'card_back_id' => $cardData['card_back_id'] ?? null,
+            'artist' => $cardData['artist'] ?? null,
+            'artist_ids' => isset($cardData['artist_ids']) ? json_encode($cardData['artist_ids']) : null,
+            'illustration_id' => $cardData['illustration_id'] ?? null,
+            'border_color' => $cardData['border_color'] ?? null,
+            'frame' => $cardData['frame'] ?? null,
+            'security_stamp' => $cardData['security_stamp'] ?? null,
+            'full_art' => $cardData['full_art'] ?? null,
+            'textless' => $cardData['textless'] ?? null,
+            'booster' => $cardData['booster'] ?? null,
+            'story_spotlight' => $cardData['story_spotlight'] ?? null,
+            'edhrec_rank' => $cardData['edhrec_rank'] ?? null,
+            'preview' => isset($cardData['preview']) ? json_encode($cardData['preview']) : null,
+            'prices' => isset($cardData['prices']) ? json_encode($cardData['prices']) : null,
+            'related_uris' => isset($cardData['related_uris']) ? json_encode($cardData['related_uris']) : null,
+            'purchase_uris' => isset($cardData['purchase_uris']) ? json_encode($cardData['purchase_uris']) : null,
+            'imgs_local' => ['small' => '', 'normal' => '', 'large' => ''],
+        ];
+
+        Card::updateOrCreate(['oracle_id' => $cardData['oracle_id']], array_filter($data));
+
+        $imageUris = $cardData['card_faces'][0]['image_uris'] ?? ($cardData['image_uris'] ?? []);
+
+        $this->saveImagesAsync($imageUris, $cardData['oracle_id']);
     }
 
     /**
@@ -155,6 +187,6 @@ class CardService implements CardServiceInterface
     {
         return Card::inRandomOrder()->take(10)->get();
     }
-
-
 }
+
+
